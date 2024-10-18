bae60de6cb902636ff6ef794d37ecdbf25dc5573
RauenLeGrange <rauenleg@gmail.com>
2024-10-18 16:01:45 +1300

    basic level built

--- game/LevelBulder.cpp
+++ game/LevelBulder.cpp
@@ -3,24 +3,43 @@
 #include <ctime>    
 
 LevelBuilder::LevelBuilder(b2World* world, float levelWidth, float levelHeight, int platformCount)
-    : m_pWorld(world), m_levelWidth(levelWidth), m_levelHeight(levelHeight), m_platformCount(platformCount) {
+    : m_pWorld(world), m_levelWidth(levelWidth), m_levelHeight(levelHeight), m_platformCount(platformCount)
+{
     std::srand(static_cast<unsigned>(std::time(0)));
 }
 
-void LevelBuilder::BuildLevel() {
+
+
+
+void LevelBuilder::BuildLevel(Renderer& renderer) {
     // Generate random platforms
     for (int i = 0; i < m_platformCount; ++i) {
         float x = static_cast<float>(std::rand() % static_cast<int>(m_levelWidth));
-        float y = static_cast<float>(std::rand() % static_cast<int>(m_levelHeight / 2));  // Only in upper half
+        float y = static_cast<float>(std::rand() % static_cast<int>(m_levelHeight - 10) + 5);  // Only in upper half
 
-        float width = 100.0f + static_cast<float>(std::rand() % 50);  // Width between 100-150
+        float width = 200.0f;  // Width between 100-150
         float height = 20.0f;  // Fixed height
 
-        GeneratePlatform(x, y, width, height);
+        GeneratePlatform(renderer, x, y, width, height);
     }
 }
 
-void LevelBuilder::GeneratePlatform(float x, float y, float width, float height) {
-    Terrain* platform = new Terrain(m_pWorld, x, y, width, height, PLATFORM);
+void LevelBuilder::GeneratePlatform(Renderer& renderer, float x, float y, float width, float height) {
+    Terrain* platform = new Terrain(m_pWorld, x/SCALE, y, width/SCALE, height*4/SCALE, PLATFORM);
     m_platforms.push_back(platform);
+    platform->Initialise(renderer);
+    printf("added platform at %f, %f\n", x, y);
+}
+
+const std::vector<Terrain*>& LevelBuilder::GetPlatforms() const
+{
+    return m_platforms;
+}
+
+
+void LevelBuilder::Draw(Renderer& renderer, Camera& camera)
+{
+    for (Terrain* platform : m_platforms) {
+        platform->Draw(renderer, camera);  
+    }
 }
